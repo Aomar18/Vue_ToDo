8a7d3d5f279f9c54ee0658105e5a98f7ad53b603
Ahmed <omar0121@umn.edu>
2018-10-26 09:35:09 -0500

    created two buttons to add/remove tasks from beginning of list

--- src/app.js
+++ src/app.js
@@ -11,12 +11,21 @@ new Vue({
     add() {
       this.tasks.push(this.input);
       this.input = "";
-      console.log('added new task')
+      console.log('added new task to end of list ')
+    },
+    addFirst(){
+      this.tasks.unshift(this.input)
+      this.input=""
+      console.log('add task to beginning of list')
     },
     erase() {
       this.tasks.pop(this.input)
       console.log('erased most recent task')
     },
+    eraseFirst() {
+      this.tasks.shift(this.input)
+      console.log('remove first')
+    }
   },
 })
 
